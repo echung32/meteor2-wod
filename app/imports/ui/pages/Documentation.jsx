77cb220c33b8c05ca2a18c2a9bf1a43d12e8153f
echung32 <143231894+echung32@users.noreply.github.com>
2023-10-25 10:48:24 -1000

    fsdf

---
+++ app/imports/ui/pages/Documentation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Documentation = () => (
+  <Container className="py-3">
+    <h1>Documentation</h1>
+    <p>First off, check the meteor-application-template-react github.io page.</p>
+    <p>If that does not answer your question, you can post a smart question to the class Discord.</p>
+    <p>If all else fails, post a question to the Meteor Forum site.</p>
+  </Container>
+);
+
+export default Documentation;
